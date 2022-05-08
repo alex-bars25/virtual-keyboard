5f754d48f6292d349c2b608bc29cf871e4797df3
alex-bars25 <alex.bars25@gmail.com>
2022-05-08 23:20:59 +0300

    feat: add input from phisical keyboard

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-import {init, switchLayout} from "./init.js";
+import {init, switchLayout} from "./utils.js";
 
 let language = 'en';
 init(language);
 
-let keys = document.querySelectorAll('.key');
-let caps = document.querySelector('.CapsLock');
+const keys = document.querySelectorAll('.key');
+const caps = document.querySelector('.CapsLock');
+const textarea = document.querySelector('.textarea');
+let before = '';
+let after = '';
+let position = textarea.selectionStart;
+const functionalKeys = ["Backspace", "Tab", "Delete", "CapsLock", "Enter", "ShiftLeft", "ShiftRight", "ControlLeft", "MetaLeft", "AltLeft", "AltRight", "ControlRight"];
 
 document.addEventListener('keydown', (e) => {
   e.preventDefault();
   for (let item of keys) {
     if (item.classList.contains(e.code)) {
       item.classList.add('active');
+      if (!functionalKeys.includes(e.code)) {
+        before += item.innerHTML;
+        position++;
+        textarea.innerHTML = `${before}${after}`
+      }
     }
   }
-    
+  
   if (e.ctrlKey && e.altKey) {
     switch (language) {
       case 'en': language = 'ru'; break;
@@ -44,6 +54,30 @@ document.addEventListener('keydown', (e) => {
     }
     switchLayout(language);
   }
+
+  if (e.code === 'Tab') {
+    before += '\t';
+    position++;
+    textarea.innerHTML = `${before}${after}`
+  }
+
+  if (e.code === 'Enter') {
+    before += '\n';
+    position++;
+    textarea.innerHTML = `${before}${after}`
+  }
+
+  if (e.code === 'Backspace') {
+    before = before.slice(0, position - 1);
+    position--;
+    textarea.innerHTML = `${before}${after}`
+  }
+
+  if (e.code === 'Delete') {
+    if (position !== textarea.innerHTML.length)
+      after = after.slice(1);
+    textarea.innerHTML = `${before}${after}`
+  }
 });
 
 document.addEventListener('keyup', (e) => {
@@ -62,3 +96,10 @@ document.addEventListener('keyup', (e) => {
     switchLayout(language);
   }
 });
+
+document.addEventListener('click', (e) => {
+  if (e.target === textarea)
+    position = textarea.selectionStart;
+  before = textarea.innerHTML.slice(0, position);
+  after = textarea.innerHTML.slice(position);
+})
