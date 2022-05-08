20ea4aecc5b9e6a0e51c69a0d077a72a565e3730
alex-bars25 <alex.bars25@gmail.com>
2022-05-08 17:13:22 +0300

    fix: fixed layout switch

--- script.js
+++ script.js
@@ -9,23 +9,41 @@ let caps = document.querySelector('.CapsLock');
 document.addEventListener('keydown', (e) => {
   e.preventDefault();
   for (let item of keys) {
-    if (item.classList.contains(e.code))
-      item.classList.add('active')
+    if (item.classList.contains(e.code)) {
+      item.classList.add('active');
+    }
   }
-  
+    
   if (e.ctrlKey && e.altKey) {
-    language === 'en' ? language = 'ru' : language = 'en'; 
+    switch (language) {
+      case 'en': language = 'ru'; break;
+      case 'ru': language = 'en'; break;
+      case 'upperEn': language = 'upperRu'; break;
+      case 'upperRu': language = 'upperEn'; break;
+    }
     switchLayout(language);
   }
 
   if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
-    language === 'en' ? switchLayout('upperEn') : switchLayout('upperRu');
+    switch (language) {
+      case 'en': language = 'upperEn'; break;
+      case 'ru': language = 'upperRu'; break;
+      case 'upperEn': language = 'en'; break;
+      case 'upperRu': language = 'ru'; break;
+    }
+    switchLayout(language);
   }
 
   if (e.code === 'CapsLock') {
     caps.classList.toggle('caps')
+    switch (language) {
+      case 'en': language = 'upperEn'; break;
+      case 'ru': language = 'upperRu'; break;
+      case 'upperEn': language = 'en'; break;
+      case 'upperRu': language = 'ru'; break;
+    }
+    switchLayout(language);
   }
-
 });
 
 document.addEventListener('keyup', (e) => {
@@ -35,6 +53,12 @@ document.addEventListener('keyup', (e) => {
   }
 
   if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
-    language === 'en' ? switchLayout('en') : switchLayout('ru');
+    switch (language) {
+      case 'en': language = 'upperEn'; break;
+      case 'ru': language = 'upperRu'; break;
+      case 'upperEn': language = 'en'; break;
+      case 'upperRu': language = 'ru'; break;
+    }
+    switchLayout(language);
   }
 });
